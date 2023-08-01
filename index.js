a104e03b8c4b69810ea204281843decce8714e08
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-01 16:41:04 -0400

    done

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-require('dotenv').config();
+require('dotenv').config()
+const fetch = require('node-fetch')
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY)
+
+const getImages = async (query) => {
+	const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+	const response = await fetch(endpoint)
+	const data = await response.json()
+	const imageUrl = data.data[0].url
+	return imageUrl
+}
+
+getImages('cat')
